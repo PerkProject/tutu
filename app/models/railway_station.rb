6322df38323f7c8999511f1530b2a8e600806edd
perkir <kspersionov@gmail.com>
2016-09-08 21:16:11 +0300

    add validate and rename associate

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,6 @@
 class RailwayStation < ActiveRecord::Base
-  has_many :trains, class_name: 'Train', foreign_key: :current_station_id
-  has_many :tickets, class_name: 'Ticket', foreign_key: :start_station_id
-  has_many :tickets, class_name: 'Ticket', foreign_key: :end_station_id
+  validates :title, presence: true
+  has_many :trains, foreign_key: :current_station_id
+  has_many :tickets_start_station, class_name: 'Ticket', foreign_key: :start_station_id
+  has_many :tickets_end_station, class_name: 'Ticket', foreign_key: :end_station_id
 end
