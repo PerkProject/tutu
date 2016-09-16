2b10be52932b99e2cfbb426ed6bcd6e4e69d9bf9
perkir <kspersionov@gmail.com>
2016-09-16 18:22:00 +0300

    light fix

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,5 +2,5 @@ class Train < ActiveRecord::Base
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   has_many :tickets
   belongs_to :route
-  has_many :carriage
+  has_many :carriages
 end
