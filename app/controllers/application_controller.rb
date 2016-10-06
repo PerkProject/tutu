38b4b3dad3cb2c2581bcd328aa2e8080af58b44e
perkir <kspersionov@gmail.com>
2016-10-07 01:50:55 +0300

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(current_user)
-    current_user.admin? ? admin_welcome_index_path : new_search_path
+    current_user.admin? ? admin_welcome_index_path : root_path
   end
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'searches#show'
+  root 'searches#new'
 
   resource :search do
     get '/' => 'searches#new'
